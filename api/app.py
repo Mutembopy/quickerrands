47cac0b3861ef3b857ffa5f7c57f913ef10d5b48
Mutembopy <163283950+Mutembopy@users.noreply.github.com>
2026-04-10 17:31:07 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,12 +1,4 @@
-from flask import Flask
-
-try:
-    from app import create_app
-    app = create_app()
-
-except Exception as e:
+def create_app(config_class=Config):
     app = Flask(__name__)
-
-    @app.route("/")
-    def error():
-        return f"CRASH: {str(e)}"
+    app.config.from_object(config_class)
+    return app
