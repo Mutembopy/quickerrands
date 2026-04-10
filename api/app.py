9d1b3e58adef39c42e428ea16242177be3eb9d4f
Mutembopy <163283950+Mutembopy@users.noreply.github.com>
2026-04-10 17:34:30 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,16 +1,7 @@
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
-    return appfrom flask import Flask
+from flask import Flask
 
-try:
-    from app import create_app
-    app = create_app()
+app = Flask(__name__)
 
-except Exception as e:
-    app = Flask(__name__)
-
-    @app.route("/")
-    def error():
-        return f"CRASH: {str(e)}"
-
+@app.route("/")
+def home():
+    return "Minimal app works"
