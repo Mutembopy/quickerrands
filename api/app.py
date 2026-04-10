b202354c27a73b4cf834fa8d640d71e9b6e1f906
Mutembopy <163283950+Mutembopy@users.noreply.github.com>
2026-04-10 17:28:20 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,10 +1,12 @@
+from flask import Flask
+
 try:
     from app import create_app
     app = create_app()
+
 except Exception as e:
-    from flask import Flask
     app = Flask(__name__)
 
     @app.route("/")
     def error():
-        return f"Startup Error: {str(e)}"
+        return f"CRASH: {str(e)}"
